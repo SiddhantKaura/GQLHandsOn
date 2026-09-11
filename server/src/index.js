dc38252aaede6d636de661930aa721dc69d839a3
Siddhant Kaura <siddhantkaura@Siddhants-Air.bbrouter>
2026-09-12 00:55:32 +0530

    feat: basic server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,22 @@
-//TODO
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { readFileSync } = require("fs");
+const path = require("path");
+const gql = require("graphql-tag");
+
+const typeDefs = gql(
+  readFileSync(path.resolve(__dirname, "./schema.graphql"), {
+    encoding: "utf-8",
+  }),
+);
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+      🚀  Server is running
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer();
