3f6f9a88db33e4013295f8633f25273145aa84ef
Siddhant Kaura <siddhantkaura@Siddhants-Air.bbrouter>
2026-09-12 01:13:03 +0530

    feat: added mocked data

--- server/src/index.js
+++ server/src/index.js
@@ -4,14 +4,44 @@ const { readFileSync } = require("fs");
 const path = require("path");
 const gql = require("graphql-tag");
 
+// For Mock Data
+const { addMocksToSchema } = require("@graphql-tools/mock");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+
 const typeDefs = gql(
   readFileSync(path.resolve(__dirname, "./schema.graphql"), {
     encoding: "utf-8",
   }),
 );
 
+const mocks = {
+  Query: () => ({
+    tracks: () => [...new Array(6)],
+  }),
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
+    author: () => {
+      return {
+        name: "Grumpy Cat",
+        avatar:
+          "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/catstrophysicist_bqfh9n_j0amow.jpg",
+      };
+    },
+    thumbnail: () =>
+      "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
+    length: () => 1210,
+    modules: () => 6,
+  }),
+};
+
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs });
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mocks,
+    }),
+  });
   const { url } = await startStandaloneServer(server);
   console.log(`
       🚀  Server is running
